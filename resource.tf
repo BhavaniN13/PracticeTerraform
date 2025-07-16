20ed98b8b278204b22c932479d0e39c8785117fc
BhavaniN13 <bhavanin.n6@gmail.com>
2025-07-16 12:12:56 +0000

    final

--- resource.tf
+++ resource.tf
@@ -1,38 +1,40 @@
+
 resource "azurerm_resource_group" "storeterra" {
-    name     = "terra"
-    location = "East US"
+  name     = "Practice-Terraform"
+  location = "East US"
 }
- 
+
 resource "azurerm_storage_account" "saterra" {
-    name                     = "terrasa"
-    resource_group_name      = azurerm_resource_group.storeterra.name
-    location                 = azurerm_resource_group.storeterra.location
-    account_tier             = "Standard"
-    account_replication_type = "LRS"
+  name                     = "terrasa"
+  resource_group_name      = azurerm_resource_group.storeterra.name
+  location                 = azurerm_resource_group.storeterra.location
+  account_tier             = "Standard"
+  account_replication_type = "LRS"
 }
- 
+
 resource "azurerm_storage_container" "sacontainer" {
-    name                  = "terra-container"
-    storage_account_id  = azurerm_storage_account.saterra.id
-    container_access_type = "private"
+  name                  = "terra-container"
+  storage_account_id    = azurerm_storage_account.saterra.id
+  container_access_type = "private"
 }
+
 resource "azurerm_virtual_network" "terra_vnet" {
-    name                = "terra-vnet"
-    address_space       = ["10.0.0.0/16"]
-    location            = azurerm_resource_group.terra.location
-    resource_group_name = azurerm_resource_group.terra.name
+  name                = "terra-vnet"
+  address_space       = ["10.0.0.0/16"]
+  location            = azurerm_resource_group.storeterra.location
+  resource_group_name = azurerm_resource_group.storeterra.name
 }
- 
+
 resource "azurerm_subnet" "subnet1" {
-    name                 = "subnet1"
-    resource_group_name  = azurerm_resource_group.terra.name
-    virtual_network_name = azurerm_virtual_network.terra_vnet.name
-    address_prefixes     = ["10.0.1.0/24"]
+  name                 = "subnet1"
+  resource_group_name  = azurerm_resource_group.storeterra.name
+  virtual_network_name = azurerm_virtual_network.terra_vnet.name
+  address_prefixes     = ["10.0.1.0/24"]
 }
- 
+
 resource "azurerm_subnet" "subnet2" {
-    name                 = "subnet2"
-    resource_group_name  = azurerm_resource_group.terra.name
-    virtual_network_name = azurerm_virtual_network.terra_vnet.name
-    address_prefixes     = ["10.0.2.0/24"]
+  name                 = "subnet2"
+  resource_group_name  = azurerm_resource_group.storeterra.name
+  virtual_network_name = azurerm_virtual_network.terra_vnet.name
+  address_prefixes     = ["10.0.2.0/24"]
 }
