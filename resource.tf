fceb0bda0165acf5e7caf5c6f572c39a7b2fd903
BhavaniN13 <bhavanin.n6@gmail.com>
2025-07-16 12:15:43 +0000

    latest

--- resource.tf
+++ resource.tf
@@ -5,7 +5,7 @@ resource "azurerm_resource_group" "storeterra" {
 }
 
 resource "azurerm_storage_account" "saterra" {
-  name                     = "terrasa"
+  name                     = "terrasa34"
   resource_group_name      = azurerm_resource_group.storeterra.name
   location                 = azurerm_resource_group.storeterra.location
   account_tier             = "Standard"
