19c8d87c8f2ec5df4c44b90edd4a5c596c9f8752
BhavaniN13 <bhavanin.n6@gmail.com>
2025-07-16 11:56:49 +0000

    initial

---
+++ resource.tf
@@ -0,0 +1,18 @@
+resource "azurerm_resource_group" "storeterra" {
+    name     = "Practice-terra"
+    location = "East US"
+}
+ 
+resource "azurerm_storage_account" "saterra" {
+    name                     = "terrasa"
+    resource_group_name      = azurerm_resource_group.storeterra.name
+    location                 = azurerm_resource_group.storeterra.location
+    account_tier             = "Standard"
+    account_replication_type = "LRS"
+}
+ 
+resource "azurerm_storage_container" "sacontainer" {
+    name                  = "terra-container"
+    storage_account_id  = azurerm_storage_account.saterra.id
+    container_access_type = "private"
+}
